af0d533c42e1f6471e465393ee8179fc9993576c
DoniaGameel <doniagameel34@gmail.com>
2024-04-04 14:35:40 +0200

    organizing client code

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -22,21 +22,8 @@ func (l *clientListener) MasterToClientSuccessNotify(ctx context.Context, req *p
 }
 
 func main() {
-	var file_name string
-	// Read file name from the user
-	fmt.Print("Enter File Name with extension: ")
-	fmt.Scanln(&file_name)
 
-	// get the file content
-	// Read file content
-	fileContent, err := os.ReadFile(file_name)
-	if err != nil {
-		// Handle error
-		fmt.Println("Error reading the file", err)
-		return
-	}
-	fmt.Printf("Read file content (first 100 bytes): %x\n", fileContent[:100]) // Print first 100 bytes (hexadecimal)
-    // Dial the master server at localhost:8080
+	// Dial the master server at localhost:8080
     conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
     if err != nil {
         fmt.Println("did not connect:", err)
@@ -65,43 +52,69 @@ func main() {
         }
     }()
 
-    // Call the RPC method
-    resp, err := c.ClientToMasterUpload(context.Background(), &pb.ClientToMasterUploadRequest{})
-    if err != nil {
-        fmt.Println("Error calling Upload to master: ", err)
-        return
-    }
-    fmt.Println("Received Upload Response from master:", resp)
-    //IpAddress := resp.GetIpAddress()
-    PortNumber := resp.GetPort()
+	var file_name string
+	// Read file name from the user
+	fmt.Print("Enter File Name with extension: ")
+	fmt.Scanln(&file_name)
+	
+	var req_type int
+	fmt.Print("Enter 1 to upload or 2 to download: ")
+	fmt.Scanln(&req_type)
 
-    connection_port := "localhost:" + PortNumber
-    conn_data, err := grpc.Dial(connection_port, grpc.WithInsecure())
-    if err != nil {
-        fmt.Println("did not connect:", err)
-        return
-    }
-    defer conn_data.Close()
-
-    // Create a client instance
-    client_2 := pb.NewServicesClient(conn_data)
-
-    // Call the RPC method
-    resp_data, err := client_2.ClientToDataKeeperUpload(context.Background(), &pb.ClientToDataKeeperUploadRequest{
-		FileName: file_name,
-		FileContent: fileContent,
-	})
-    if err != nil {
-        fmt.Println("Error calling Upload to data node: ", err)
-        return
-    }
-    fmt.Println("Successfully Uploaded to the datanode:", resp_data)
-
-    // Signal that success notification is received
-    wg.Done()
-
-    // Wait for success notification
-    wg.Wait()
-    fmt.Println("Successfully uploaded file. Disconnecting...")
+	if req_type == 1{
+		// Upload logic
+		// Read file content
+		fileContent, err := os.ReadFile(file_name)
+		if err != nil {
+			// Handle error
+			fmt.Println("Error reading the file", err)
+			return
+		}
+		// Call the RPC method
+		resp, err := c.ClientToMasterUpload(context.Background(), &pb.ClientToMasterUploadRequest{})
+		if err != nil {
+			fmt.Println("Error calling Upload to master: ", err)
+			return
+		}
+		fmt.Println("Received Upload Response from master:", resp)
+		//IpAddress := resp.GetIpAddress()
+		PortNumber := resp.GetPort()
+	
+		connection_port := "localhost:" + PortNumber
+		conn_data, err := grpc.Dial(connection_port, grpc.WithInsecure())
+		if err != nil {
+			fmt.Println("did not connect:", err)
+			return
+		}
+		defer conn_data.Close()
+	
+		// Create a client instance
+		client_2 := pb.NewServicesClient(conn_data)
+	
+		// Call the RPC method
+		resp_data, err := client_2.ClientToDataKeeperUpload(context.Background(), &pb.ClientToDataKeeperUploadRequest{
+			FileName: file_name,
+			FileContent: fileContent,
+		})
+		if err != nil {
+			fmt.Println("Error calling Upload to data node: ", err)
+			return
+		}
+		fmt.Println("Successfully Uploaded to the datanode:", resp_data)
+	
+		// Signal that success notification is received
+		wg.Done()
+	
+		// Wait for success notification
+		wg.Wait()
+		fmt.Println("Successfully uploaded file. Disconnecting...")
+		
+	} else if req_type ==2 {
+		// download logic
+		fmt.Println("You requested type 2.")
+	} else {
+		fmt.Println("Unknown request type:", req_type)
+		return
+	}
 }
 
